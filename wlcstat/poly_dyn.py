f914eb4ca9919e8141eec34ba97151751bdd07c0
ajspakow <ajspakow@stanford.edu>
2021-02-15 22:07:35 -0700

    Updated poly_dyn

--- wlcstat/poly_dyn.py
+++ wlcstat/poly_dyn.py
@@ -1,77 +1,86 @@
-"""Rouse polymer, analytical results.
+r"""
+Polymer dynamics module - Rouse polymer, analytical results.
 
 Notes
 -----
-There are two parameterizations of the "Rouse" polymer that are commonly used,
-and they use the same variable name for two different things.
-
-In one, N is the number of Kuhn lengths, and in the other, N is the number of
-beads, each of which can represent an arbitrary number of Kuhn lengths.
 """
 import numpy as np
 from numba import jit
 import matplotlib.pyplot as plt
-# import mpmath
-
+import scipy.special as sp
+from wlcstat.util.mittag_leffler_master.mittag_leffler import *
 from functools import lru_cache
 from pathlib import Path
 import os
 
 
-@jit
-def rouse_mode(p, n, N=1):
-    """Eigenbasis for Rouse model.
-
-    Indexed by p, depends only on position n/N along the polymer of length N.
-    N=1 by default.
-
-    Weber, Phys Rev E, 2010 (Eq 14)"""
-    p = np.atleast_1d(p)
-    phi = np.sqrt(2)*np.cos(p*np.pi*n/N)
-    phi[p == 0] = 1
-    return phi
-
-
-@jit(nopython=True)
-def rouse_mode_coef(p, b, N, kbT=1):
-    """k_p: Weber Phys Rev E 2010, after Eq. 18."""
-    # alternate: k*pi**2/N * p**2, i.e. k = 3kbT/b**2
-    return 3*np.pi**2*kbT/(N*b**2)*p**2
-
-
-@jit(nopython=True)
-def kp_over_kbt(p : float, b : float, N : float):
-    """k_p/(k_B T) : "non-dimensionalized" k_p is all that's needed for most
-    formulas, e.g. MSD."""
-    return (3*np.pi*np.pi)/(N*b*b) * p*p
-
-
 @jit(nopython=True)
 def linear_mid_msd(t, b, N, D, num_modes=20000):
-    """
-    modified from Weber Phys Rev E 2010, Eq. 24.
+    r"""
+    Calculate the MSD for the midpoint on a Rouse polymer
+
+    Parameters
+    ----------
+    t : float array
+        Time in seconds
+    b : float
+        Kuhn length (microns)
+    N : float
+        Chain length (in Kuhn lengths)
+    D : float
+        Diffusivity (microns ** 2 / second)
+    num_modes : int
+        Number of normal modes used in the calculation
+
+    Returns
+    -------
+    msd : float array (size len(t))
+        Calculated MSD for a Rouse polymer
+
     """
     msd = np.zeros_like(t)
     for p in range(1, num_modes+1):
-        k2p_norm = kp_over_kbt(2*p, b, N)
+        k2p_norm = (3 * np.pi * np.pi) / (N * b * b) * (2 * p) ** 2
         msd += (1 / k2p_norm) * (1 - np.exp(-k2p_norm * (D / N) * t))
+
     return 12 * msd + 6 * D * t / N
 
 
-@jit(nopython=True)
-def gaussian_G(r, N, b):
-    """Green's function of a Gaussian chain at N Kuhn lengths of separation,
-    given a Kuhn length of b"""
-    r2 = np.power(r, 2)
-    return np.power(3/(2*np.pi*b*b*N), 3/2)*np.exp(-(3/2)*r2/(N*b*b))
+def linear_mid_msd_subpoly(t, b, N, D, alpha=1, num_modes=20000):
+    r"""
+    Calculate the MSD for the midpoint on a subdiffusive Rouse polymer
 
+    Parameters
+    ----------
+    t : float array
+        Time in seconds
+    b : float
+        Kuhn length (microns)
+    N : float
+        Chain length (in Kuhn lengths)
+    D : float
+        Diffusivity (microns ** 2 / second)
+    alpha : float
+        MSD scaling exponent
+    num_modes : int
+        Number of normal modes used in the calculation
 
-@jit(nopython=True)
-def gaussian_Ploop(a, N, b):
-    """Looping probability for two loci on a Gaussian chain N kuhn lengths
-    apart, when the Kuhn length is b, and the capture radius is a"""
-    Nb2 = N*b*b
-    return spycial.erf(a*np.sqrt(3/2/Nb2)) - a*np.sqrt(6/np.pi/Nb2)/np.exp(3*a*a/2/Nb2)
+    Returns
+    -------
+    msd : float array (size len(t))
+        Calculated MSD for a Rouse polymer
+
+    """
+    msd = np.zeros_like(t)
+    for p in range(1, num_modes+1):
+        k2p_norm = (3 * np.pi * np.pi) / (N * b * b) * (2 * p) ** 2
+        exp_coeff = k2p_norm * D / N / sp.gamma(3 - alpha)
+        cp = ml(-exp_coeff * t ** alpha, alpha)
+        msd += (1 / k2p_norm) * (1 - cp)
+    d_coef = np.sin(alpha * np.pi) / (np.pi * (1 - 0.5 * alpha) * (1 - alpha) * alpha)
+    msd = 12 * msd + 6 * D * d_coef * t ** alpha / N
+
+    return msd
 
 
 @jit(nopython=True)
@@ -125,6 +134,7 @@ def confined_G(r, rp, N, b, a, n_max=100, l_max=50):
     lm_term[~lm_mask] = 0
     # now broadcast and sum
     G = np.sum(ln_term[None,:,:] * lm_term[:,:,None])
+
     return G
 confined_G.zl_n = None
 
@@ -287,6 +297,88 @@ def linear_msd_confine_plateau(Nmono, N, b=1, k_conf=1, num_modes=20000):
     return msd_plateau
 
 
+def linear_mscd_subpoly(t, D, Ndel, N, b=1, alpha=1, num_modes=20000):
+    r"""
+    Compute mscd for two points on a linear polymer.
+
+    Parameters
+    ----------
+    t : (M,) float, array_like
+        Times at which to evaluate the MSCD
+    D : float
+        Diffusion coefficient, (in desired output length units). Equal to
+        :math:`k_BT/\xi` for :math:`\xi` in units of "per Kuhn length".
+    Ndel : float
+        Distance from the last linkage site to the measured site. This ends up
+        being (1/2)*separation between the loci (in Kuhn lengths).
+    N : float
+        The full lengh of the linear polymer (in Kuhn lengths).
+    b : float
+        The Kuhn length (in desired length units).
+    alpha : float
+        Subdiffusive powerlaw scaling exponent for diffusion
+    num_modes : int
+        how many Rouse modes to include in the sum
+
+    Returns
+    -------
+    mscd : (M,) np.array<float>
+        result
+    """
+    mscd = np.zeros_like(t)
+
+    k1 = 3 * np.pi ** 2 / (N * (b ** 2))
+    sum_coeff = 48 / k1
+    exp_coeff = k1 * D / N / sp.gamma(3 - alpha)
+    sin_coeff = np.pi * Ndel / N
+
+    for p in range(1, num_modes+1, 2):
+        cp = ml(-exp_coeff * (p ** 2) * t ** alpha, alpha)
+        mscd += (1/p**2) * (1 - cp) * np.sin(sin_coeff*p)**2
+
+    return sum_coeff * mscd
+
+
+def ring_mscd_subpoly(t, D, Ndel, N, b=1, alpha=1, num_modes=20000):
+    r"""
+    Compute mscd for two points on a ring.
+
+    Parameters
+    ----------
+    t : (M,) float, array_like
+        Times at which to evaluate the MSCD.
+    D : float
+        Diffusion coefficient, (in desired output length units). Equal to
+        :math:`k_BT/\xi` for :math:`\xi` in units of "per Kuhn length".
+    Ndel : float
+        (1/2)*separation between the loci on loop (in Kuhn lengths)
+    N : float
+        full length of the loop (in Kuhn lengths)
+    b : float
+        The Kuhn length, in desired output length units.
+    alpha : float
+        Subdiffusive powerlaw scaling exponent for diffusion
+    num_modes : int
+        How many Rouse modes to include in the sum.
+
+    Returns
+    -------
+    mscd : (M,) np.array<float>
+        result
+    """
+    mscd = np.zeros_like(t)
+
+    k1 = 12 * np.pi ** 2 / (N * (b ** 2))
+    sum_coeff = 48 / k1
+    exp_coeff = k1 * D / N / sp.gamma(3 - alpha)
+    sin_coeff = 2 * np.pi * Ndel / N
+
+    for p in range(1, num_modes+1):
+        cp = ml(-exp_coeff * (p ** 2) * t ** alpha, alpha)
+        mscd += (1 / p ** 2) * (1 - cp) * np.sin(sin_coeff * p) ** 2
+    return sum_coeff * mscd
+
+
 def end_to_end_corr(t, D, N, num_modes=10000):
     """Doi and Edwards, Eq. 4.35"""
     mscd = np.zeros_like(t)
@@ -386,19 +478,6 @@ def draw_cells(linkages, min_y=0.05, max_y=0.95, locus_frac=ura_locus_frac, cent
     return ax, all_closest_links
 
 
-#chrv_size_bp = 576874
-#location_ura_bp = np.mean([116167, 116970])
-
-#chrv_size_kuhn = 1165
-#kuhn_length = 0.015  # um
-#chrv_size_effective_um = chrv_size_kuhn*kuhn_length
-#location_ura_effective_um = location_ura_bp*(chrv_size_effective_um/chrv_size_bp)
-
-#nuc_radius_um = 1.3  # Average of het5 msd convex hull distribution
-#sim_nuc_radius_um = 1
-#sim_D = 0.02  # um^2/s
-
-
 def model_mscd(t, linkages, label_loc=location_ura_effective_um, chr_size=chrv_size_effective_um,
                nuc_radius=sim_nuc_radius_um, b=kuhn_length, D=sim_D, num_modes=10000):
     r"""
@@ -458,6 +537,70 @@ def model_mscd(t, linkages, label_loc=location_ura_effective_um, chr_size=chrv_s
         N = 2 * (linkages[i] - linkages[i - 1])
 
     mscd_model = mscd_func(t, D=D, Ndel=Ndel, N=N, b=b, num_modes=num_modes)
+    mscd_model = np.minimum(mscd_model, (nuc_radius ** 2) * np.ones(len(mscd_model)))
+
+    return mscd_model
+
+
+def model_mscd_subpoly(t, linkages, label_loc=location_ura_effective_um, chr_size=chrv_size_effective_um,
+               nuc_radius=sim_nuc_radius_um, b=kuhn_length, D=sim_D, alpha=1, num_modes=10000):
+    r"""
+    Calculate the MSCD for the model of linked chromosomes
+
+    Parameters
+    ----------
+    t : float array
+        Time in seconds
+    linkages : float array
+        List of the link positions between the homologous chromosomes
+    label_loc : float
+        Location of the fluorescent label along the chromosome (microns)
+    chr_size : float
+        Length of the chromosome (microns)
+    nuc_radius : float
+        Radius of the nucleus (microns)
+    b : float
+        Kuhn length (microns)
+    D : float
+        Diffusivity (microns ** 2 / second)
+    num_modes : int
+        Number of normal modes used in the calculation
+
+    Returns
+    -------
+    mscd_model : float array (size len(t))
+        Calculated MSCD (microns ** 2) for the model with defined linkages
+
+    """
+
+    linkages = np.array(linkages) / b
+    chr_size = chr_size / b
+    label_loc = label_loc / b
+
+    # Evaluate the MSCD if there are no linkages
+    if len(linkages) == 0:
+        mscd_model = 2 * linear_mid_msd_subpoly(t, b, chr_size, D, num_modes)
+        for i in range(len(t)):
+            if mscd_model[i] > nuc_radius ** 2:
+                mscd_model[i] = nuc_radius ** 2
+        return mscd_model
+
+    # Evaluate the MSCD if there are linkages between the chromosomes
+    i = np.searchsorted(linkages, label_loc)
+    if i == 0:
+        mscd_func = linear_mscd_subpoly
+        Ndel = linkages[0] - label_loc
+        N = 2 * linkages[0]
+    elif i == len(linkages):
+        mscd_func = linear_mscd_subpoly
+        Ndel = label_loc - linkages[-1]
+        N = 2 * (chr_size - linkages[-1])
+    else:
+        mscd_func = ring_mscd_subpoly
+        Ndel = linkages[i] - label_loc
+        N = 2 * (linkages[i] - linkages[i - 1])
+
+    mscd_model = mscd_func(t, D=D, Ndel=Ndel, N=N, b=b, alpha=alpha, num_modes=num_modes)
     mscd_model = np.minimum(mscd_model, (nuc_radius ** 2) * np.ones(len(mscd_model)))
 
     return mscd_model
@@ -604,3 +747,42 @@ def model_plateau(linkages, label_loc=location_ura_effective_um, chr_size=chrv_s
         mscd_plateau = nuc_radius ** 2
 
     return mscd_plateau
+
+
+def model_mscd_subpoly_simple(t, linkages, label_loc=location_ura_effective_um, chr_size=chrv_size_effective_um,
+               nuc_radius=sim_nuc_radius_um, b=kuhn_length, D=sim_D, alpha=1, num_modes=10000):
+    r"""
+    Calculate the MSCD for the model of linked chromosomes
+
+    Parameters
+    ----------
+    t : float array
+        Time in seconds
+    linkages : float array
+        List of the link positions between the homologous chromosomes
+    label_loc : float
+        Location of the fluorescent label along the chromosome (microns)
+    chr_size : float
+        Length of the chromosome (microns)
+    nuc_radius : float
+        Radius of the nucleus (microns)
+    b : float
+        Kuhn length (microns)
+    D : float
+        Diffusivity (microns ** 2 / second)
+    num_modes : int
+        Number of normal modes used in the calculation
+
+    Returns
+    -------
+    mscd_model : float array (size len(t))
+        Calculated MSCD (microns ** 2) for the model with defined linkages
+
+    """
+
+    mscd_plateau = model_plateau(linkages, label_loc, chr_size, nuc_radius, b)
+    mscd_model = 0.809 * 4 * b * (D * t ** alpha) ** 0.5
+
+    mscd_model = np.minimum(mscd_model, mscd_plateau * np.ones(len(mscd_model)))
+
+    return mscd_model
